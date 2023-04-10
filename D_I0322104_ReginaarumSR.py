630dd9812b30fe6d52dd073dfddcccc4f634b6fa
reginaarum <126972904+reginaarum@users.noreply.github.com>
2023-04-10 21:45:56 +0700

    Tugas 5

---
+++ D_I0322104_ReginaarumSR.py
@@ -0,0 +1,20 @@
+input data 
+
+T_45/84
+Harga_rumah = 550000000
+Uang_muka_(DP) = 100000000
+Jangka_waktu = 5 tahun
+suku_bunga/bulan = 0,5 %
+
+T_55/112
+Harga_rumah = 850000000
+Uang_muka_(DP) = 50000000
+Jangka_waktu = 10 tahun
+Suku_bunga/bulan = 0,5 %
+
+T_60/120
+Harga_rumah = 950000000
+Uang_muka = 200000000     
+Jangka_waktu = 8 tahun
+Suku_bunga/bulan = 0,5%
+
